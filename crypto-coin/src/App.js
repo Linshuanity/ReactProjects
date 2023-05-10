65e5c4553088b5e16fcab7fd8f1a07fde05143fd
TingChunSu <p11922006@csie.ntu.edu.tw>
2023-05-10 23:45:00 +0800

    user login IndexedDB

--- crypto-coin/src/App.js
+++ crypto-coin/src/App.js
@@ -1,6 +1,6 @@
 import Link from '@mui/material/Link';
-import React, { useEffect, useState } from 'react';
-import Cookies from 'universal-cookie';
+import React, { useEffect, useState, useContext } from 'react';
+//import Cookies from 'universal-cookie';
 import Game from './Game';
 import Home from './Home';
 import SignIn from './login/SignIn';
@@ -9,6 +9,7 @@ import Post from './Post';
 import Profile from './Profile';
 import Gallery, { posts } from './Gallery';
 import { Route, Routes } from 'react-router-dom';
+import { UserContext } from "./context/UserContext";
 
 const profiles = [
   { id: 1, name: 'Linshuanity', photo: 'https://picsum.photos/100' },
@@ -17,13 +18,16 @@ const profiles = [
 ];
 
 function App() {
+  const { user } = useContext(UserContext);
   const [currentPage, setCurrentPage] = useState('');
   const getUser = () => {
-    const cookies = new Cookies();
-    if (cookies.get('user') === undefined) {
-      return '';
-    }
-    return cookies.get('user');
+    // const cookies = new Cookies();
+    // if (cookies.get('user') === undefined) {
+    //   return '';
+    // }
+    // return cookies.get('user');
+    console.log(user);
+    return user ? user : {};
   };
 
   useEffect(() => {
@@ -42,12 +46,12 @@ function App() {
         </ul>
       </nav>
       <Routes>
-        <Route path="/SignUp" element={<SignUp/>} />
-        <Route path="/SignIn" element={<SignIn/>} />
-        <Route path="/game" element={<Game/>} />
-        <Route path="/profile/:id" element={<Profile profiles={profiles}/>} />
-        <Route path="/gallery" element={<Gallery/>} />
-        <Route path="/" element={<Home/>} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/profile/:id" element={<Profile profiles={profiles} />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/" element={<Home />} />
         {posts.map((post) => (
           <Route key={post.id} path={`/posts/${post.id}`} element={<Post post={post} />} />
         ))}
