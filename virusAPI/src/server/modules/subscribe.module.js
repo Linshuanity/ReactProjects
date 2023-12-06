aebe6fe655774dc5cbac3e5eb0d35ff0c1808423
Su Ting Chun <p11922006@ntu.edu.tw>
2023-12-06 21:52:58 +0800

    1206connection

--- virusAPI/src/server/modules/subscribe.module.js
+++ virusAPI/src/server/modules/subscribe.module.js
@@ -29,6 +29,7 @@ const getFriends = (userId) => {
     });
   });
 };
+
 const isSubscribed = (suberId, subedId) => {
   return new Promise((resolve, reject) => {
     connectionPool.getConnection((connectionError, connection) => { // 資料庫連線
@@ -112,8 +113,7 @@ const addRemoveSubscribe = async (req, res, isFriend) => {
   });
 };
 
-
-/* User  POST 新增 */
+/* User POST 新增 */
 const createSubscribe = (insertValues) => {
   const userId = insertValues.user_id;
   const friendId = insertValues.friend_id;
@@ -122,28 +122,37 @@ const createSubscribe = (insertValues) => {
     connectionPool.getConnection((connectionError, connection) => { // 資料庫連線
       if (connectionError) {
         reject(connectionError); // 若連線有問題回傳錯誤
-      } else {
-        const query = isDelete ? `DELETE from subscribes WHERE subscriber_id = ${userId} and subscribed_id = ${friendId}` : `INSERT INTO subscribes VALUES (DEFAULT, ${userId}, ${friendId}, DEFAULT)`;
-        console.log(isDelete === false);
-        console.log(query);
-        connection.query(query, (error, result) => { // User資料表寫入一筆資料
-          if (error) {
-            console.error('SQL error: ', error);
-            reject(error); // 寫入資料庫有問題時回傳錯誤
-          } else if (result.affectedRows === 1) {
-              const getSubscribeList = `SELECT s.subscribed_id as _id, u.user_name as name, u.user_image_path as picturePath FROM subscribes as s JOIN virus_platform_user as u ON u.user_id = s.subscribed_id where s.subscriber_id = ?`;
-              connection.query(getSubscribeList, [userId], (error, result) => {
-                if (error) {
-                  console.error('Error retrieving friends:', error);
-                  reject(error);
-                }
-
-                resolve(result); // 寫入成功回傳寫入id
-              })
-          }
-          connection.release();
-        });
+        return;
       }
+
+      const query = isDelete 
+        ? `DELETE from subscribes WHERE subscriber_id = ? and subscribed_id = ?`
+        : `INSERT INTO subscribes VALUES (DEFAULT, ?, ?, DEFAULT)`;
+      
+      connection.query(query, [userId, friendId], (error, result) => { // User資料表寫入一筆資料
+        if (error) {
+          console.error('SQL error: ', error);
+          connection.release();
+          reject(error); // 寫入資料庫有問題時回傳錯誤
+          return;
+        }
+
+        if (result.affectedRows === 1) {
+          const getSubscribeList = `SELECT s.subscribed_id as _id, u.user_name as name, u.user_image_path as picturePath FROM subscribes as s JOIN virus_platform_user as u ON u.user_id = s.subscribed_id where s.subscriber_id = ?`;
+          connection.query(getSubscribeList, [userId], (error, friends) => {
+            connection.release();
+            if (error) {
+              console.error('Error retrieving friends:', error);
+              reject(error);
+              return;
+            }
+            resolve(friends); // 寫入成功回傳好友列表
+          });
+        } else {
+          connection.release();
+          reject(new Error('No rows affected'));
+        }
+      });
     });
   });
 };
