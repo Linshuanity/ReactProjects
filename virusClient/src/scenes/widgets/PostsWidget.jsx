ac530a1556928434e19f77e5ff8f6b3c507b3f55
linshuanity <edison4987@gmail.com>
2024-10-15 23:06:03 +0800

    New feature - search by content

--- virusClient/src/scenes/widgets/PostsWidget.jsx
+++ virusClient/src/scenes/widgets/PostsWidget.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Tab, Tabs, Typography } from '@mui/material'
+import { Tab, Tabs, Typography, IconButton, Button, useTheme } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
 import PostWidget from './PostWidget'
 
 const PostsWidget = ({ userId, isProfile = false }) => {
+    const { palette } = useTheme()
     const [mode, setMode] = useState(0)
     const dispatch = useDispatch()
     const [posts, setPosts] = useState([])
+    const [keyword, setKeyword] = useState('')
     const token = useSelector((state) => state.token)
     const loggedInUserId = useSelector((state) => state.user._id)
 
@@ -19,6 +22,9 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
     const getPosts = async () => {
         try {
+            if (mode > 3) {
+                return;
+            }
             const response = await fetch(`${apiEndpoint}/posts/all`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -54,6 +60,28 @@ const PostsWidget = ({ userId, isProfile = false }) => {
         }
     }
 
+    const handleSearch = async () => {
+        try {
+            const response = await fetch(`${apiEndpoint}/posts/search`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    login_user: loggedInUserId,
+                    keyword: keyword,
+                }),
+            })
+            if (!response.ok) throw new Error('Network response was not ok')
+            const data = await response.json()
+            setPosts(data)
+        } catch (error) {
+            console.error('Error fetching posts:', error)
+        }
+    }
+
+    const handleChange = (e) => {
+        setKeyword(e.target.value)
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             await getPosts()
@@ -70,7 +98,31 @@ const PostsWidget = ({ userId, isProfile = false }) => {
                     <Tab label="My post" />
                     <Tab label="My collection" />
                     {userId == loggedInUserId && <Tab label="My order" />}
+                    <Tab label="Search" />
                 </Tabs>
+                {mode === 4 && (
+                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
+                        <input
+                            value={keyword}
+                            onChange={handleChange}
+                            type="text"
+                            style={{
+                                width: '100%',
+                                padding: '8px',
+                                border: '1px solid #ccc',
+                                borderRadius: '4px',
+                                outline: 'none',
+                                resize: 'none',
+                                backgroundColor: '#fff', // Ensure visibility with a solid background
+                                color: 'grey',
+                            }}
+                            placeholder="Search your query..."
+                        />
+                        <IconButton onClick={handleSearch} style={{ marginLeft: '8px' }}>
+                            <SearchIcon />
+                        </IconButton>
+                    </div>
+                )}
             </div>
             {posts.map((post) => (
                 <PostWidget
